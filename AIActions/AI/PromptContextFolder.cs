0d3d658966d0fd8bf02ae712cb71f68b69bcdc8c
Dorian399 <dorianetflorian@gmaiL.com>
2025-09-12 16:11:37 +0200

    Add PromptContextFolder class.

---
+++ AIActions/AI/PromptContextFolder.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIActions.AI
+{
+    internal class PromptContextFolder
+    {
+        public string PythonVersion { get; private set; }
+        public string OS { get; private set; }
+        public string FilesPath { get; private set; }
+        public string FilesAmount { get; private set; }
+        public string FilesExtensions {  get; private set; }
+        public string FilesList { get; private set; }
+        public string Prompt {  get; private set; }
+
+        public PromptContextFolder(string filesPath, string prompt)
+        {
+            Prompt = prompt ?? " ";
+            FilesPath = filesPath ?? "Path missing.";
+            OS = RuntimeInformation.OSDescription ?? "Unknown OS";
+
+            PythonVersion = "3.10"; // TO DO
+
+            string[] files = Directory.GetFiles(FilesPath);
+            string[] directories = Directory.GetDirectories(FilesPath);
+
+            int filesAmount = files.Count();
+            int directoriesAmount = directories.Count();
+            string filesAndDirectoriesAmount = filesAmount.ToString() + " files, " + directoriesAmount.ToString() + " directories";
+
+            FilesAmount = filesAndDirectoriesAmount ?? "0 files, 0 directories";
+
+            List<string> filesList = new List<string>();
+            HashSet<string> extensions = new HashSet<string>();
+
+            int maxFiles = 20;
+            foreach(string file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                if(maxFiles > 0)
+                {
+                    filesList.Add(fileName);
+                    maxFiles--;
+                }
+
+                string ext = Path.GetExtension(file);
+                if (!extensions.Contains(ext))
+                {
+                    extensions.Add(ext);
+                }
+
+            }
+
+            FilesList = String.Join("; ", filesList);
+            FilesExtensions = String.Join("; ", extensions);
+
+        }
+
+    }
+}
