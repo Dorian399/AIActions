8434e0a08fcac43401af97f0162cb5d53029b06b
Dorian399 <dorianetflorian@gmaiL.com>
2025-09-14 13:45:19 +0200

    Add early checks and config loading to Main

--- AIActions/Program.cs
+++ AIActions/Program.cs
@@ -1,3 +1,9 @@
+using AIActions.AI;
+using AIActions.Configs;
+using AIActions.ExternalData;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
 namespace AIActions
 {
     internal static class Program
@@ -6,12 +12,41 @@ namespace AIActions
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static async Task Main(string[] args)
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+
+            if (!File.Exists(Paths.PythonExecutable))
+            {
+                MessageBox.Show("Missing Python executable, check your /data/python directory or reinstall the program.");
+                return;
+            }
+
+            if (!File.Exists(Paths.PipExecutable))
+            {
+                MessageBox.Show("Missing pip module for Python, check your /data/python directory or reinstall the program.");
+                return;
+            }
+
+            string? workFolder=null;
+            if (args.Length > 0)
+            {
+                workFolder = args[0];
+            }
+
+            string configName = "gemini_flash.json"; // TO DO, Use persistent user settings.
+            string configFullPath = Path.Combine(Paths.ConfigFilesFolder, configName);
+
+            ConfigLoader loader = new ConfigLoader();
+            ParsedConfig parsedConfig = await loader.LoadFromFile(configFullPath);
+
             ApplicationConfiguration.Initialize();
-            Application.Run(new PromptWindow());
+
+            PromptWindow window = new PromptWindow(workFolder, parsedConfig);
+            window.ShouldHide = true;
+
+            Application.Run(window);
         }
     }
 }
