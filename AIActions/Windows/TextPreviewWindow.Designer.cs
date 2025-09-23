2449f51f6a363a7740f22f9934aaa4dc4d925979
Dorian399 <dorianetflorian@gmaiL.com>
2025-09-23 11:14:43 +0200

    Add TextPreviewWindow Window

---
+++ AIActions/Windows/TextPreviewWindow.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace AIActions.Windows
+{
+    partial class TextPreviewWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TextPreview = new RichTextBox();
+            SuspendLayout();
+            // 
+            // TextPreview
+            // 
+            TextPreview.Dock = DockStyle.Fill;
+            TextPreview.ForeColor = SystemColors.WindowText;
+            TextPreview.Location = new Point(0, 0);
+            TextPreview.Name = "TextPreview";
+            TextPreview.ReadOnly = true;
+            TextPreview.Size = new Size(518, 498);
+            TextPreview.TabIndex = 0;
+            TextPreview.Text = "";
+            // 
+            // TextPreviewWindow
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(518, 498);
+            Controls.Add(TextPreview);
+            Name = "TextPreviewWindow";
+            Text = "Code preview";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private RichTextBox TextPreview;
+    }
+}
---
+++ AIActions/Windows/TextPreviewWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AIActions.Windows
+{
+    public partial class TextPreviewWindow : Form
+    {
+        public TextPreviewWindow(string text,string title)
+        {
+            InitializeComponent();
+            if (!String.IsNullOrWhiteSpace(title))
+                this.Text = title;
+            TextPreview.Text = text;
+        }
+    }
+}
