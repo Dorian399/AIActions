9c35f2a9649e33292337d9514305b8959e6663bf
Dorian399 <dorianetflorian@gmaiL.com>
2025-09-23 11:13:50 +0200

    Add TextPreviewButton Class

---
+++ AIActions/Elements/TextPreviewButton.cs
@@ -0,0 +1,35 @@
+﻿using AIActions.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class TextPreviewButton : Button
+{
+    private string _previewText;
+    private string _previewTitle;
+    private TextPreviewWindow? _childWindow;
+
+    private void RemoveChildWindow(object sender, FormClosedEventArgs e)
+    {
+        _childWindow = null;
+    }
+
+    public void SetPreviewText(string text,string title)
+    {
+        _previewText = text;
+        _previewTitle = title;
+    }
+
+    protected override void OnClick(EventArgs e)
+    {
+        if (_previewText != null && _childWindow == null)
+        {
+            _childWindow = new TextPreviewWindow(_previewText,_previewTitle);
+            _childWindow.Show();
+            _childWindow.FormClosed += RemoveChildWindow;
+        }
+        base.OnClick(e);
+    }
+}
