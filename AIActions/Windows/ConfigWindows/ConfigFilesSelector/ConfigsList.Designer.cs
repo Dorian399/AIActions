1f8235b6025e978f82874d7b407172e6adff4dc0
Dorian399 <dorianetflorian@gmaiL.com>
2025-10-01 14:05:58 +0200

    Add ConfigsList Control

---
+++ AIActions/Windows/ConfigWindows/ConfigFilesSelector/ConfigsList.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace AIActions.Windows.ConfigWindows
+{
+    partial class ConfigsList
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            MainLayout = new TableLayoutPanel();
+            HeaderLayout = new TableLayoutPanel();
+            ConfigComboBox = new ComboBox();
+            LoadedConfigsLabel = new Label();
+            VariablesPanel = new Panel();
+            MainLayout.SuspendLayout();
+            HeaderLayout.SuspendLayout();
+            SuspendLayout();
+            // 
+            // MainLayout
+            // 
+            MainLayout.ColumnCount = 1;
+            MainLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            MainLayout.Controls.Add(HeaderLayout, 0, 0);
+            MainLayout.Controls.Add(VariablesPanel, 0, 2);
+            MainLayout.Dock = DockStyle.Fill;
+            MainLayout.Location = new Point(0, 0);
+            MainLayout.Margin = new Padding(0);
+            MainLayout.Name = "MainLayout";
+            MainLayout.RowCount = 3;
+            MainLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 39F));
+            MainLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 21F));
+            MainLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 207F));
+            MainLayout.Size = new Size(555, 301);
+            MainLayout.TabIndex = 0;
+            // 
+            // HeaderLayout
+            // 
+            HeaderLayout.ColumnCount = 2;
+            HeaderLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 29.0090084F));
+            HeaderLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 70.99099F));
+            HeaderLayout.Controls.Add(ConfigComboBox, 1, 0);
+            HeaderLayout.Controls.Add(LoadedConfigsLabel, 0, 0);
+            HeaderLayout.Dock = DockStyle.Fill;
+            HeaderLayout.Location = new Point(0, 0);
+            HeaderLayout.Margin = new Padding(0);
+            HeaderLayout.Name = "HeaderLayout";
+            HeaderLayout.RowCount = 1;
+            HeaderLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            HeaderLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            HeaderLayout.Size = new Size(555, 39);
+            HeaderLayout.TabIndex = 0;
+            // 
+            // ConfigComboBox
+            // 
+            ConfigComboBox.Dock = DockStyle.Fill;
+            ConfigComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            ConfigComboBox.FormattingEnabled = true;
+            ConfigComboBox.Location = new Point(167, 6);
+            ConfigComboBox.Margin = new Padding(6, 6, 20, 6);
+            ConfigComboBox.Name = "ConfigComboBox";
+            ConfigComboBox.Size = new Size(368, 28);
+            ConfigComboBox.TabIndex = 0;
+            // 
+            // LoadedConfigsLabel
+            // 
+            LoadedConfigsLabel.AutoSize = true;
+            LoadedConfigsLabel.Dock = DockStyle.Fill;
+            LoadedConfigsLabel.Font = new Font("Segoe UI", 10F);
+            LoadedConfigsLabel.Location = new Point(3, 0);
+            LoadedConfigsLabel.Name = "LoadedConfigsLabel";
+            LoadedConfigsLabel.Size = new Size(155, 39);
+            LoadedConfigsLabel.TabIndex = 1;
+            LoadedConfigsLabel.Text = "Available configs:";
+            LoadedConfigsLabel.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // VariablesPanel
+            // 
+            VariablesPanel.AutoScroll = true;
+            VariablesPanel.Dock = DockStyle.Fill;
+            VariablesPanel.Location = new Point(3, 63);
+            VariablesPanel.Name = "VariablesPanel";
+            VariablesPanel.Padding = new Padding(5);
+            VariablesPanel.Size = new Size(549, 235);
+            VariablesPanel.TabIndex = 2;
+            // 
+            // ConfigsList
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(MainLayout);
+            Name = "ConfigsList";
+            Size = new Size(555, 301);
+            Load += ConfigsList_Loaded;
+            MainLayout.ResumeLayout(false);
+            HeaderLayout.ResumeLayout(false);
+            HeaderLayout.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel MainLayout;
+        private TableLayoutPanel HeaderLayout;
+        private ComboBox ConfigComboBox;
+        private Label LoadedConfigsLabel;
+        private Panel VariablesPanel;
+    }
+}
---
+++ AIActions/Windows/ConfigWindows/ConfigFilesSelector/ConfigsList.cs
@@ -0,0 +1,56 @@
+﻿using AIActions.Configs;
+using AIActions.ExternalData;
+using AIActions.Windows.ConfigWindows.ConfigFilesSelector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AIActions.Windows.ConfigWindows
+{
+    public partial class ConfigsList : UserControl
+    {
+        private Dictionary<string, ParsedConfig> _configsCache;
+        private Dictionary<int, ParsedConfig> _loadedConfigs;
+        public ConfigsList()
+        {
+            InitializeComponent();
+        }
+
+        private async void ConfigsList_Loaded(object sender, EventArgs e)
+        {
+            ConfigComboBox.Items.Clear();
+            _loadedConfigs = new Dictionary<int, ParsedConfig>();
+
+            SortedDictionary<string, string> configFiles = Paths.ConfigFiles;
+
+            ConfigLoader configLoader = new ConfigLoader();
+
+            int index = 0;
+            foreach (var kvp in configFiles) {
+                ParsedConfig? config;
+
+                if (_configsCache.ContainsKey(kvp.Value)) {
+                    config = _configsCache[kvp.Value];
+                }
+                else
+                {
+                    config = await configLoader.LoadFromFile(kvp.Value);
+                }
+
+                if (config == null)
+                    continue;
+                ConfigComboBox.Items.Insert(index,config.Name);
+                _configsCache[kvp.Value] = config;
+                _loadedConfigs[index] = config;
+                index++;
+            }
+        }
+    }
+}
