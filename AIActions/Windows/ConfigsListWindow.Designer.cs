7868dda8199006a9d88c8f30b98ebd183f39d677
Dorian399 <dorianetflorian@gmaiL.com>
2025-10-05 17:38:58 +0200

    Finish ConfigsList window Add minimum size Add working confirm button

--- AIActions/Windows/ConfigsListWindow.Designer.cs
+++ AIActions/Windows/ConfigsListWindow.Designer.cs
@@ -28,30 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            ConfigsList = new AIActions.Windows.ConfigWindows.ConfigsList();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            configsList1 = new AIActions.Windows.ConfigWindows.ConfigsList();
+            confirmButton = new Button();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
-            // ConfigsList
+            // tableLayoutPanel1
             // 
-            ConfigsList.Dock = DockStyle.Fill;
-            ConfigsList.Location = new Point(0, 0);
-            ConfigsList.Name = "ConfigsList";
-            ConfigsList.Size = new Size(537, 432);
-            ConfigsList.TabIndex = 0;
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(configsList1, 0, 0);
+            tableLayoutPanel1.Controls.Add(confirmButton, 0, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Margin = new Padding(0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 45F));
+            tableLayoutPanel1.Size = new Size(594, 333);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // configsList1
+            // 
+            configsList1.Location = new Point(3, 3);
+            configsList1.Name = "configsList1";
+            configsList1.Size = new Size(588, 282);
+            configsList1.TabIndex = 0;
+            configsList1.OnConfigChosen = ConfigChosen;
+            // 
+            // confirmButton
+            // 
+            confirmButton.Dock = DockStyle.Right;
+            confirmButton.Enabled = false;
+            confirmButton.Location = new Point(494, 294);
+            confirmButton.Margin = new Padding(6);
+            confirmButton.Name = "confirmButton";
+            confirmButton.Padding = new Padding(3);
+            confirmButton.Size = new Size(94, 33);
+            confirmButton.TabIndex = 1;
+            confirmButton.Text = "Confirm";
+            confirmButton.UseVisualStyleBackColor = true;
+            confirmButton.Click += ConfirmButton_Clicked;
             // 
             // ConfigsListWindow
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(537, 432);
-            Controls.Add(ConfigsList);
+            ClientSize = new Size(594, 333);
+            Controls.Add(tableLayoutPanel1);
+            MinimumSize = new Size(552, 275);
             Name = "ConfigsListWindow";
-            Text = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Config Setup";
+            FormClosed += ConfigsListWindow_Closed;
+            tableLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private ConfigWindows.ConfigsList ConfigsList;
+        private TableLayoutPanel tableLayoutPanel1;
+        private ConfigWindows.ConfigsList configsList1;
+        private Button confirmButton;
     }
 }
--- AIActions/Windows/ConfigsListWindow.cs
+++ AIActions/Windows/ConfigsListWindow.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using AIActions.Configs;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -17,5 +19,22 @@ namespace AIActions.Windows
             InitializeComponent();
         }
 
+        private void ConfigChosen()
+        {
+            confirmButton.Enabled = true;
+        }
+
+        private void ConfirmButton_Clicked(object sender, EventArgs e)
+        {
+            // Confirm button just closes the window as all data is saved dynamically.
+            this.Close();
+        }
+
+        private async void ConfigsListWindow_Closed(object sender, EventArgs e)
+        {
+            ConfigLoader loader = new ConfigLoader();
+            ParsedConfig? parsedConfig = await loader.LoadFromAppSettings();
+            Program.MainWindow.UpdateData(configFile: parsedConfig, shouldPopUp: true);
+        }
     }
 }
