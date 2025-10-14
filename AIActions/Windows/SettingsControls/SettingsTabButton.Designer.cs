948012a30d2876686e675670c304d59851c854bc
Dorian399 <dorianetflorian@gmaiL.com>
2025-10-14 21:45:01 +0200

    Add hover borders and all the logic to SettingsTabButton

---
+++ AIActions/Windows/SettingsControls/SettingsTabButton.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace AIActions.Windows.SettingsControls
+{
+    partial class SettingsTabButton
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 32.7354279F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 67.26457F));
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Controls.Add(label1, 1, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(107, 37);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = Properties.Resources.SettingsIcon;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Margin = new Padding(0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Padding = new Padding(2);
+            pictureBox1.Size = new Size(35, 37);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Fill;
+            label1.Font = new Font("Segoe UI", 10F);
+            label1.Location = new Point(38, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 37);
+            label1.TabIndex = 1;
+            label1.Text = "Setting";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // SettingsTabButton
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            Controls.Add(tableLayoutPanel1);
+            Margin = new Padding(0, 0, 0, 3);
+            Name = "SettingsTabButton";
+            Size = new Size(107, 37);
+            Load += SettingsTabButton_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private PictureBox pictureBox1;
+        private Label label1;
+    }
+}
---
+++ AIActions/Windows/SettingsControls/SettingsTabButton.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AIActions.Windows.SettingsControls
+{
+    public partial class SettingsTabButton : UserControl
+    {
+        private List<Panel> _borderPanels = new List<Panel>();
+        private DockStyle[] _borderAlignments = {DockStyle.Top, DockStyle.Left,DockStyle.Bottom,DockStyle.Right};
+        private int _borderHoverThickness = 1;
+        private int _borderActiveThickness = 1;
+        private bool _isActive = false;
+        private Color _defaultBackColor;
+        private TabControl? _tabControl;
+
+        public TabControl? TabControl
+        {
+            get => _tabControl;
+            set
+            {
+                if (_tabControl != null)
+                {
+                    _tabControl.SelectedIndexChanged -= TabControl_SelectedIndexChanged;
+                }
+
+                _tabControl = value;
+
+                if (_tabControl != null)
+                {
+                    _tabControl.SelectedIndexChanged += TabControl_SelectedIndexChanged;
+                }
+
+            }
+        }
+        public Color HoverBackgroundColor { get; set; } = Color.FromArgb(223,236,249);
+        public Color BorderColor { get; set; } = SystemColors.Highlight;
+        public Color ActiveBackgroundColor { get; set; } = SystemColors.GradientInactiveCaption;
+        public Padding ImageMargin { get { return pictureBox1.Margin; } set { pictureBox1.Margin = value; } }
+        public string LabelText { 
+            get 
+            { return label1.Text; } 
+            set { label1.Text = value; } 
+        }
+        public SettingsTabButton()
+        {
+            InitializeComponent();
+
+            // Create border
+            for(int i=0;i<_borderAlignments.Length;i++)
+            {
+                Panel borderPanel = new Panel();
+                borderPanel.Dock = _borderAlignments[i];
+                if( i%2 == 0 )
+                    borderPanel.Size = new Size(Size.Width,_borderHoverThickness);
+                else
+                    borderPanel.Size= new Size(_borderHoverThickness, Size.Height);
+                borderPanel.BackColor = BorderColor;
+                borderPanel.Visible = false;
+                borderPanel.Enabled = false;
+                this.Controls.Add(borderPanel);
+                borderPanel.BringToFront();
+                _borderPanels.Add(borderPanel);
+            }
+
+            _defaultBackColor = BackColor;
+
+            MouseEnter += SettingsTabButton_MouseEnter;
+            MouseLeave += SettingsTabButton_MouseLeave;
+            MouseClick += SettingsTabButton_MouseClick;
+
+            pictureBox1.MouseEnter += SettingsTabButton_MouseEnter;
+            pictureBox1.MouseLeave += SettingsTabButton_MouseLeave;
+            pictureBox1.MouseClick += SettingsTabButton_MouseClick;
+
+            label1.MouseEnter += SettingsTabButton_MouseEnter;
+            label1.MouseLeave += SettingsTabButton_MouseLeave;
+            label1.MouseClick += SettingsTabButton_MouseClick;
+
+        }
+
+        private void SettingsTabButton_Load(object sender, EventArgs e)
+        {
+            TabControl_SelectedIndexChanged(this, EventArgs.Empty);
+        }
+
+        private void SetBorderThickness(int thickness)
+        {
+            foreach(Panel panel in _borderPanels)
+            {
+                if(panel.Dock == DockStyle.Top || panel.Dock == DockStyle.Bottom)
+                    panel.Size = new Size(Size.Width,thickness);
+                else
+                    panel.Size = new Size(thickness, Size.Height);
+            }
+        }
+
+        private void SetBorderVisible(bool visible)
+        {
+            foreach (Panel panel in _borderPanels)
+            {
+                panel.Visible = visible;
+            }
+        }
+
+        private void SettingsTabButton_MouseEnter(object? sender, EventArgs e)
+        {
+            if (_isActive)
+                return;
+            SetBorderThickness(_borderHoverThickness);
+            SetBorderVisible(true);
+            BackColor = HoverBackgroundColor;
+        }
+
+        private void SettingsTabButton_MouseLeave(object? sender, EventArgs e)
+        {
+            if (_isActive)
+            {
+                BackColor = ActiveBackgroundColor;
+                SetBorderThickness(_borderActiveThickness);
+                SetBorderVisible(true);
+            }
+            else 
+            {
+                SetBorderVisible(false);
+                BackColor = _defaultBackColor;
+            }
+        }
+
+        private void SettingsTabButton_MouseClick(object? sender, MouseEventArgs e)
+        {
+            if (TabControl == null)
+                return;
+
+            foreach(TabPage page in TabControl.TabPages)
+            {
+                if (page.Name == label1.Text)
+                {
+                    TabControl.SelectedIndex = TabControl.TabPages.IndexOf(page);
+                }
+            }
+        }
+
+        private void TabControl_SelectedIndexChanged(object? sender, EventArgs e)
+        {
+            if (TabControl == null)
+                return;
+            TabPage? curTab = TabControl.SelectedTab;
+            if (curTab != null && curTab.Name == label1.Text)
+            {
+                _isActive = true;
+                SetBorderThickness(_borderActiveThickness);
+                SetBorderVisible(true);
+                BackColor = ActiveBackgroundColor;
+            }
+            else
+            {
+                _isActive = false;
+                SetBorderVisible(false);
+                BackColor= _defaultBackColor;
+            }
+        }
+    }
+}
