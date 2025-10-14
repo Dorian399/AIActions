8de5f3e369551239d31465c9cdce1a87d50130b1
Dorian399 <dorianetflorian@gmaiL.com>
2025-10-14 21:43:59 +0200

    Add new setting icon

--- AIActions/Properties/Resources.Designer.cs
+++ AIActions/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace AIActions.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap SettingsIcon {
+            get {
+                object obj = ResourceManager.GetObject("SettingsIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
