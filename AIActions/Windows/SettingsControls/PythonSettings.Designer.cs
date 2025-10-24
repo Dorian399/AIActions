d9f8e4592676c3b9f449725e4521dea40197f3b1
Dorian399 <dorianetflorian@gmaiL.com>
2025-10-24 12:26:12 +0200

    Add logic for pip packages size and Python version in PythonSettings control

--- AIActions/Windows/SettingsControls/PythonSettings.Designer.cs
+++ AIActions/Windows/SettingsControls/PythonSettings.Designer.cs
@@ -30,17 +30,17 @@
         {
             tableLayoutPanel1 = new TableLayoutPanel();
             flowLayoutPanel1 = new FlowLayoutPanel();
-            label1 = new Label();
-            button1 = new Button();
-            button2 = new Button();
+            pipSizeLabel = new Label();
+            removeAllButton = new Button();
+            removeSelectedButton = new Button();
             groupBox1 = new GroupBox();
-            checkedListBox1 = new CheckedListBox();
+            pipPackages = new CheckedListBox();
             groupBox2 = new GroupBox();
-            richTextBox1 = new RichTextBox();
+            STDOut = new RichTextBox();
             flowLayoutPanel2 = new FlowLayoutPanel();
-            label2 = new Label();
-            label3 = new Label();
-            button3 = new Button();
+            pythonVersionLabel = new Label();
+            pythonScriptsAmountLabel = new Label();
+            pythonHistoryButton = new Button();
             tableLayoutPanel1.SuspendLayout();
             flowLayoutPanel1.SuspendLayout();
             groupBox1.SuspendLayout();
@@ -68,9 +68,9 @@
             // 
             // flowLayoutPanel1
             // 
-            flowLayoutPanel1.Controls.Add(label1);
-            flowLayoutPanel1.Controls.Add(button1);
-            flowLayoutPanel1.Controls.Add(button2);
+            flowLayoutPanel1.Controls.Add(pipSizeLabel);
+            flowLayoutPanel1.Controls.Add(removeAllButton);
+            flowLayoutPanel1.Controls.Add(removeSelectedButton);
             flowLayoutPanel1.Dock = DockStyle.Fill;
             flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
             flowLayoutPanel1.Location = new Point(0, 0);
@@ -79,40 +79,39 @@
             flowLayoutPanel1.Size = new Size(255, 99);
             flowLayoutPanel1.TabIndex = 0;
             // 
-            // label1
+            // pipSizeLabel
             // 
-            label1.AutoSize = true;
-            label1.Dock = DockStyle.Left;
-            label1.Location = new Point(3, 0);
-            label1.Name = "label1";
-            label1.Padding = new Padding(3, 0, 0, 0);
-            label1.Size = new Size(135, 20);
-            label1.TabIndex = 0;
-            label1.Text = "Pip packages size: ";
+            pipSizeLabel.AutoSize = true;
+            pipSizeLabel.Dock = DockStyle.Left;
+            pipSizeLabel.Location = new Point(3, 0);
+            pipSizeLabel.Name = "pipSizeLabel";
+            pipSizeLabel.Size = new Size(132, 20);
+            pipSizeLabel.TabIndex = 0;
+            pipSizeLabel.Text = "Pip packages size: ";
             // 
-            // button1
+            // removeAllButton
             // 
-            button1.Font = new Font("Segoe UI", 8F);
-            button1.Location = new Point(3, 23);
-            button1.Name = "button1";
-            button1.Size = new Size(148, 29);
-            button1.TabIndex = 1;
-            button1.Text = "Remove all packages";
-            button1.UseVisualStyleBackColor = true;
+            removeAllButton.Font = new Font("Segoe UI", 8F);
+            removeAllButton.Location = new Point(3, 23);
+            removeAllButton.Name = "removeAllButton";
+            removeAllButton.Size = new Size(148, 29);
+            removeAllButton.TabIndex = 1;
+            removeAllButton.Text = "Remove all packages";
+            removeAllButton.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // removeSelectedButton
             // 
-            button2.Font = new Font("Segoe UI", 8F);
-            button2.Location = new Point(3, 58);
-            button2.Name = "button2";
-            button2.Size = new Size(186, 29);
-            button2.TabIndex = 2;
-            button2.Text = "Remove selected packages";
-            button2.UseVisualStyleBackColor = true;
+            removeSelectedButton.Font = new Font("Segoe UI", 8F);
+            removeSelectedButton.Location = new Point(3, 58);
+            removeSelectedButton.Name = "removeSelectedButton";
+            removeSelectedButton.Size = new Size(186, 29);
+            removeSelectedButton.TabIndex = 2;
+            removeSelectedButton.Text = "Remove selected packages";
+            removeSelectedButton.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(checkedListBox1);
+            groupBox1.Controls.Add(pipPackages);
             groupBox1.Dock = DockStyle.Fill;
             groupBox1.Location = new Point(3, 102);
             groupBox1.Name = "groupBox1";
@@ -121,21 +120,21 @@
             groupBox1.TabStop = false;
             groupBox1.Text = "Pip packages";
             // 
-            // checkedListBox1
+            // pipPackages
             // 
-            checkedListBox1.CheckOnClick = true;
-            checkedListBox1.Dock = DockStyle.Fill;
-            checkedListBox1.FormattingEnabled = true;
-            checkedListBox1.Items.AddRange(new object[] { "test1", "test2", "test3", "test4", "test5" });
-            checkedListBox1.Location = new Point(3, 23);
-            checkedListBox1.Name = "checkedListBox1";
-            checkedListBox1.Size = new Size(243, 170);
-            checkedListBox1.Sorted = true;
-            checkedListBox1.TabIndex = 0;
+            pipPackages.CheckOnClick = true;
+            pipPackages.Dock = DockStyle.Fill;
+            pipPackages.FormattingEnabled = true;
+            pipPackages.Items.AddRange(new object[] { "test1", "test2", "test3", "test4", "test5" });
+            pipPackages.Location = new Point(3, 23);
+            pipPackages.Name = "pipPackages";
+            pipPackages.Size = new Size(243, 170);
+            pipPackages.Sorted = true;
+            pipPackages.TabIndex = 0;
             // 
             // groupBox2
             // 
-            groupBox2.Controls.Add(richTextBox1);
+            groupBox2.Controls.Add(STDOut);
             groupBox2.Dock = DockStyle.Fill;
             groupBox2.Location = new Point(258, 102);
             groupBox2.Name = "groupBox2";
@@ -145,21 +144,21 @@
             groupBox2.TabStop = false;
             groupBox2.Text = "Output window";
             // 
-            // richTextBox1
+            // STDOut
             // 
-            richTextBox1.Dock = DockStyle.Fill;
-            richTextBox1.Location = new Point(3, 23);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.ReadOnly = true;
-            richTextBox1.Size = new Size(288, 167);
-            richTextBox1.TabIndex = 0;
-            richTextBox1.Text = "";
+            STDOut.Dock = DockStyle.Fill;
+            STDOut.Location = new Point(3, 23);
+            STDOut.Name = "STDOut";
+            STDOut.ReadOnly = true;
+            STDOut.Size = new Size(288, 167);
+            STDOut.TabIndex = 0;
+            STDOut.Text = "";
             // 
             // flowLayoutPanel2
             // 
-            flowLayoutPanel2.Controls.Add(label2);
-            flowLayoutPanel2.Controls.Add(label3);
-            flowLayoutPanel2.Controls.Add(button3);
+            flowLayoutPanel2.Controls.Add(pythonVersionLabel);
+            flowLayoutPanel2.Controls.Add(pythonScriptsAmountLabel);
+            flowLayoutPanel2.Controls.Add(pythonHistoryButton);
             flowLayoutPanel2.Dock = DockStyle.Fill;
             flowLayoutPanel2.FlowDirection = FlowDirection.TopDown;
             flowLayoutPanel2.Location = new Point(255, 0);
@@ -168,36 +167,34 @@
             flowLayoutPanel2.Size = new Size(300, 99);
             flowLayoutPanel2.TabIndex = 3;
             // 
-            // label2
+            // pythonVersionLabel
             // 
-            label2.AutoSize = true;
-            label2.Dock = DockStyle.Left;
-            label2.Location = new Point(3, 0);
-            label2.Margin = new Padding(3, 0, 3, 3);
-            label2.Name = "label2";
-            label2.Size = new Size(112, 20);
-            label2.TabIndex = 0;
-            label2.Text = "Python version: ";
+            pythonVersionLabel.Dock = DockStyle.Left;
+            pythonVersionLabel.Location = new Point(3, 0);
+            pythonVersionLabel.Margin = new Padding(3, 0, 3, 3);
+            pythonVersionLabel.Name = "pythonVersionLabel";
+            pythonVersionLabel.Size = new Size(294, 20);
+            pythonVersionLabel.TabIndex = 0;
+            pythonVersionLabel.Text = "Python version: ";
             // 
-            // label3
+            // pythonScriptsAmountLabel
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(3, 23);
-            label3.Margin = new Padding(3, 0, 3, 3);
-            label3.Name = "label3";
-            label3.Size = new Size(132, 20);
-            label3.TabIndex = 1;
-            label3.Text = "Scripts generated: ";
+            pythonScriptsAmountLabel.Location = new Point(3, 23);
+            pythonScriptsAmountLabel.Margin = new Padding(3, 0, 3, 3);
+            pythonScriptsAmountLabel.Name = "pythonScriptsAmountLabel";
+            pythonScriptsAmountLabel.Size = new Size(297, 20);
+            pythonScriptsAmountLabel.TabIndex = 1;
+            pythonScriptsAmountLabel.Text = "Scripts generated: ";
             // 
-            // button3
+            // pythonHistoryButton
             // 
-            button3.Font = new Font("Segoe UI", 8F);
-            button3.Location = new Point(3, 49);
-            button3.Name = "button3";
-            button3.Size = new Size(140, 29);
-            button3.TabIndex = 2;
-            button3.Text = "Show scripts history";
-            button3.UseVisualStyleBackColor = true;
+            pythonHistoryButton.Font = new Font("Segoe UI", 8F);
+            pythonHistoryButton.Location = new Point(3, 49);
+            pythonHistoryButton.Name = "pythonHistoryButton";
+            pythonHistoryButton.Size = new Size(140, 29);
+            pythonHistoryButton.TabIndex = 2;
+            pythonHistoryButton.Text = "Show scripts history";
+            pythonHistoryButton.UseVisualStyleBackColor = true;
             // 
             // PythonSettings
             // 
@@ -206,13 +203,13 @@
             Controls.Add(tableLayoutPanel1);
             Name = "PythonSettings";
             Size = new Size(555, 301);
+            Load += PythonSettings_Load;
             tableLayoutPanel1.ResumeLayout(false);
             flowLayoutPanel1.ResumeLayout(false);
             flowLayoutPanel1.PerformLayout();
             groupBox1.ResumeLayout(false);
             groupBox2.ResumeLayout(false);
             flowLayoutPanel2.ResumeLayout(false);
-            flowLayoutPanel2.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -220,16 +217,16 @@
 
         private TableLayoutPanel tableLayoutPanel1;
         private FlowLayoutPanel flowLayoutPanel1;
-        private Label label1;
-        private Button button1;
-        private Button button2;
+        private Label pipSizeLabel;
+        private Button removeAllButton;
+        private Button removeSelectedButton;
         private GroupBox groupBox1;
-        private CheckedListBox checkedListBox1;
+        private CheckedListBox pipPackages;
         private GroupBox groupBox2;
-        private RichTextBox richTextBox1;
+        private RichTextBox STDOut;
         private FlowLayoutPanel flowLayoutPanel2;
-        private Label label2;
-        private Label label3;
-        private Button button3;
+        private Label pythonVersionLabel;
+        private Label pythonScriptsAmountLabel;
+        private Button pythonHistoryButton;
     }
 }
--- AIActions/Windows/SettingsControls/PythonSettings.cs
+++ AIActions/Windows/SettingsControls/PythonSettings.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AIActions.ExternalData;
+using AIActions.Python;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +18,76 @@ namespace AIActions.Windows.SettingsControls
         {
             InitializeComponent();
         }
+
+        private static long GetDirectorySize(DirectoryInfo directory)
+        {
+            if (directory == null || !directory.Exists)
+            {
+                throw new DirectoryNotFoundException("Directory does not exist.");
+            }
+            long size = 0;
+            try
+            {
+                size += directory.GetFiles().Sum(file => file.Length);
+                size += directory.GetDirectories().Sum(GetDirectorySize);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine($"We do not have access to {directory}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"We encountered an error processing {directory}: ");
+                Console.WriteLine($"{ex.Message}");
+            }
+            return size;
+        }
+
+        private void UpdatePipSizeLabel()
+        {
+            string pipSizeText = pipSizeLabel.Text;
+            pipSizeLabel.Text = pipSizeText + "Calculating...";
+            try
+            {
+                long dirSize = GetDirectorySize(new DirectoryInfo(Paths.PipPackagesFolder));
+                float readableSize;
+
+                if (dirSize < 1024)
+                {
+                    pipSizeLabel.Text = pipSizeText + dirSize.ToString() + " Bytes";
+                }
+                else if (dirSize < 1024 * 1024)
+                {
+                    pipSizeLabel.Text = pipSizeText + (dirSize / 1024.0).ToString("F2") + " KB";
+                }
+                else if (dirSize < 1024 * 1024 * 1024)
+                {
+                    pipSizeLabel.Text = pipSizeText + (dirSize / (1024.0 * 1024)).ToString("F2") + " MB";
+                }
+                else
+                {
+                    pipSizeLabel.Text = pipSizeText + (dirSize / (1024.0 * 1024 * 1024)).ToString("F2") + " GB";
+                }
+            }
+            catch (Exception ex)
+            {
+                pipSizeLabel.Text = pipSizeText + "0 MB";
+            }
+        }
+
+        private void PythonSettings_Load(object sender, EventArgs e)
+        {
+            CancellationToken token = default;
+            SettingsWindow? settingsWindow = (SettingsWindow?)FindForm();
+            if(settingsWindow != null && settingsWindow.CancellationTokenSource != null)
+                token = settingsWindow.CancellationTokenSource.Token;
+
+            UpdatePipSizeLabel();
+
+            PythonInfo.GetPythonVersion(token).ContinueWith(t => { pythonVersionLabel.Text += t.Result; }, TaskContinuationOptions.ExecuteSynchronously);
+
+        }
+
+
     }
 }
