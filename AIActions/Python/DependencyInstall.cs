753fcb90c95bdb281c560d8dee0aabfc68d684ac
Dorian399 <dorianetflorian@gmaiL.com>
2025-11-11 16:17:23 +0100

    Add misisng newline to DependencyInstall

--- AIActions/Python/DependencyInstall.cs
+++ AIActions/Python/DependencyInstall.cs
@@ -16,20 +16,20 @@ namespace AIActions.Python
             if(packages.Length == 0) {
                 return;
             }
-            OnOutput?.Invoke("Installing dependencies...");
+            OnOutput?.Invoke("Installing dependencies...\n");
             ProcessExec executor = new ProcessExec();
             foreach (string package in packages) {
                 token.ThrowIfCancellationRequested();
                 await InstallPackage(package,executor, token);
             }
-            OnOutput?.Invoke("Finished installing dependencies.");
+            OnOutput?.Invoke("Finished installing dependencies.\n");
         }
 
         public async Task<bool> InstallPackage(string package,ProcessExec executor,CancellationToken token=default)
         {
             try
             {
-                OnOutput?.Invoke("Installing " + package);
+                OnOutput?.Invoke("Installing " + package+"\n");
                 int code = await executor.StartAsync(Paths.PythonExecutable,"-m pip install --target \""+Paths.PipPackagesFolder+"\" " + package,token);
                 if (code != 0) {
                     // If install failed try to install package without the specific version
@@ -39,15 +39,15 @@ namespace AIActions.Python
                         string package_noversion = package.Substring(0, index);
                         return await InstallPackage(package_noversion, executor, token);
                     }
-                    OnOutput?.Invoke("Package: "+package+" failed to install.");
+                    OnOutput?.Invoke("Package: "+package+" failed to install.\n");
                     return false;
                 }
-                OnOutput?.Invoke("Installed: "+package);
+                OnOutput?.Invoke("Installed: "+package+"\n");
                 return true;
             }
             catch(OperationCanceledException e)
             {
-                OnOutput?.Invoke("Operation canceled.");
+                OnOutput?.Invoke("Operation canceled.\n");
                 throw;
             }
             catch (Exception e) {
